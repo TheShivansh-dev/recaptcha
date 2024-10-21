0b78c8a3dfef45cb5872528e86cfbbfa918f124c
Shivansh <118342696+TheShivansh-dev@users.noreply.github.com>
2024-10-21 17:44:52 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Install required system dependencies for Playwright
-apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libxkbcommon-x11-0 libgtk-3-0 libgbm-dev
